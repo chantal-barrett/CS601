ad676329b14f62e65715a40fd367d39ce0de7a44
Chantal Barrett <chantalbarrett@Chantals-Air.fios-router.home>
2022-11-22 19:27:08 -0500

    Homework four updates

---
+++ HW4/README.md
@@ -0,0 +1,29 @@
+# Assignment Four
+
+A form that submits user inputs.
+
+## Description
+
+This assignment uses HTML5, CSS, and JavaScript to create a web site with a form that asks the user to enter their first name, last name, facilitator, and select what they are inquiring about and their preferred methods of contact. The program validates that the user entered their name and a valid facilitator. 
+
+
+### How I Exceeded Requirements
+
+In trying my best to get an A on this assigment, I did the following:
+- Styled the page using CSS and made it responsive
+- Made error messages different depending on the user's input
+
+## Getting Started
+
+### Executing program
+
+* To run this program, simply open the index.html file in a web browser of your choosing. I have also hosted this website here: https://chantalbarrett.netlify.app/hw4/index.html
+
+## Author
+
+Chantal Barrett
+
+## Version History
+
+* 0.1
+    * Initial Release
--- HW4/script.js
+++ HW4/script.js
@@ -1,101 +1,114 @@
-function validateForm() {
+document.addEventListener("DOMContentLoaded", function() {
 
-    // Get needed values to validate
-    const firstName = document.getElementById("firstName");
-    const lastName = document.getElementById("lastName");
-    const facilitator = document.getElementById("facilitator");
+    // Add event listener for when the form is submitted
+    document.querySelector(".form").addEventListener("submit", validateForm);
 
-    // Hide all error fields
-    let errors = document.querySelectorAll(".error");
-    for (i = 0; i < errors.length; i++) {
-        errors[i].classList.add("hidden");
-    }
+    // Validates the content being submitted in the form
+    function validateForm(event) {
 
-    // If there is an error, we will use these to focus on the first incorrect field;
-    let isValid = true;
-    let firstInvalidField = "";
+        // Get needed values to validate
+        const firstName = document.getElementById("firstName");
+        const lastName = document.getElementById("lastName");
+        const facilitator = document.getElementById("facilitator");
+        
+        // Declare needed variable
+        const errorClass = "field-error";
 
-    // If first name consists of less than two characters
-    if(firstName.value.length < 2) {
-        // Show the correct error message
-        document.getElementById("firstNameError").innerText = "Please ensure first name is at least two characters long";
-        document.getElementById("firstNameError").classList.remove("hidden");
-
-        // The first field to focus on will be this field
-        firstInvalidField = firstName;
-
-        // Set isValid to false, as this is not a valid input
-        isValid = false;
-    }
-
-    // If last name consists of less than two characters
-    if(lastName.value.length < 2) {
-        // Show the correct error message
-        document.getElementById("lastNameError").innerText = "Please ensure last name is at least two characters long";
-        document.getElementById("lastNameError").classList.remove("hidden");
-
-        // If there is still no invalid field, this will be to the first field to focus on
-        firstInvalidField = (firstInvalidField == "") ? lastName : firstInvalidField;
-
-        // Set isValid to false, as this is not a valid input
-        isValid = false;
-    }
-
-    // If first name consists of anything other than alpha characters
-    if(!/^[A-Za-z]*$/.test(firstName.value)) {
-        // Show the correct error message
-        // If there is an existing error message, combine the two messages
-        if (document.getElementById("firstNameError").innerText.length > 0) {
-            document.getElementById("firstNameError").innerText = document.getElementById("firstNameError").innerText + " and consists of only alpha characters";
+        // Hide all error fields and clear their text
+        let errors = document.querySelectorAll(".field-error");
+        for (i = 0; i < errors.length; i++) {
+            errors[i].classList.remove(errorClass);
+            errors[i].querySelector(".error").innerText = "";
         }
-        else {
-            document.getElementById("firstNameError").innerText = "Please ensure you only enter alpha characters";
+
+        // If there is an error, we will use these to focus on the first incorrect field;
+        let isValid = true;
+        let firstInvalidField = "";
+
+        // If first name consists of less than two characters
+        if(firstName.value.length < 2) {
+            // Show the correct error message
+            document.getElementById("firstNameError").innerText = "Please ensure first name is at least two characters long";
+            document.getElementById("firstNameError").parentElement.classList.add(errorClass);
+
+            // The first field to focus on will be this field
+            firstInvalidField = firstName;
+
+            // Set isValid to false, as this is not a valid input
+            isValid = false;
         }
-        document.getElementById("firstNameError").classList.remove("hidden");
 
-        // If there is still no invalid field, this will be to the first field to focus on
-        firstInvalidField = (firstInvalidField == "") ? firstName : firstInvalidField;
+        // If last name consists of less than two characters
+        if(lastName.value.length < 2) {
+            // Show the correct error message
+            document.getElementById("lastNameError").innerText = "Please ensure last name is at least two characters long";
+            document.getElementById("lastNameError").parentElement.classList.add(errorClass);
 
-        // Set isValid to false, as this is not a valid input
-        isValid = false;
-    }
+            // If there is still no invalid field, this will be to the first field to focus on
+            firstInvalidField = (firstInvalidField == "") ? lastName : firstInvalidField;
 
-    // If last name consists of anything other than alpha characters
-    if(!/^[A-Za-z]*$/.test(lastName.value)) {
-        // Show the correct error message
-        // If there is an existing error message, combine the two messages
-        if (document.getElementById("lastNameError").innerText.length > 0) {
-            document.getElementById("lastNameError").innerText = document.getElementById("lasttNameError").innerText + " and consists of only alpha characters";
+            // Set isValid to false, as this is not a valid input
+            isValid = false;
         }
-        else {
-            document.getElementById("lastNameError").innerText = "Please ensure you only enter alpha characters";
+
+        // If first name consists of anything other than alpha characters
+        if(!/^[A-Za-z]*$/.test(firstName.value)) {
+            // Show the correct error message
+            // If there is an existing error message, combine the two messages
+            if (document.getElementById("firstNameError").innerText.length > 0) {
+                document.getElementById("firstNameError").innerText = document.getElementById("firstNameError").innerText + " and consists of only alpha characters";
+            }
+            else {
+                document.getElementById("firstNameError").innerText = "Please ensure you only enter alpha characters";
+            }
+            document.getElementById("firstNameError").parentElement.classList.add(errorClass);
+
+            // If there is still no invalid field, this will be to the first field to focus on
+            firstInvalidField = (firstInvalidField == "") ? firstName : firstInvalidField;
+
+            // Set isValid to false, as this is not a valid input
+            isValid = false;
         }
-        document.getElementById("lastNameError").classList.remove("hidden");
 
-        // If there is still no invalid field, this will be to the first field to focus on
-        firstInvalidField = (firstInvalidField == "") ? lastName : firstInvalidField;
+        // If last name consists of anything other than alpha characters
+        if(!/^[A-Za-z]*$/.test(lastName.value)) {
+            // Show the correct error message
+            // If there is an existing error message, combine the two messages
+            if (document.getElementById("lastNameError").innerText.length > 0) {
+                document.getElementById("lastNameError").innerText = document.getElementById("lasttNameError").innerText + " and consists of only alpha characters";
+            }
+            else {
+                document.getElementById("lastNameError").innerText = "Please ensure you only enter alpha characters";
+            }
+            document.getElementById("lastNameError").parentElement.classList.add(errorClass);
 
-        // Set isValid to false, as this is not a valid input
-        isValid = false;
+            // If there is still no invalid field, this will be to the first field to focus on
+            firstInvalidField = (firstInvalidField == "") ? lastName : firstInvalidField;
+
+            // Set isValid to false, as this is not a valid input
+            isValid = false;
+        }
+
+        // If the facilitator's name is not a valid facilitator
+        if (facilitator.value != "Josh" && facilitator.value != "Christian H"  && facilitator.value != "Christian C"  && facilitator.value != "Behdad" && facilitator.value != "Fazil") {
+            // Show the correct error message
+            document.getElementById("facilitatorError").innerText = "Please enter a valid facilator";
+            document.getElementById("facilitatorError").parentElement.classList.add(errorClass);
+
+            // If there is still no invalid field, this will be to the first field to focus on
+            firstInvalidField = (firstInvalidField == "") ? facilitator : firstInvalidField;
+
+            // Set isValid to false, as this is not a valid input
+            isValid = false;
+        }
+
+        // If there is an invalid field
+        if (!isValid) {
+            // Focus on the first invalid field
+            firstInvalidField.focus();
+
+            // Prevent the form from being submitted
+            event.preventDefault();
+        }
     }
-
-    // If the facilitator's name is not a valid facilitator
-    if (facilitator.value != "Josh" && facilitator.value != "Christian H"  && facilitator.value != "Christian C"  && facilitator.value != "Behdad" && facilitator.value != "Fazil") {
-        // Show the correct error message
-        document.getElementById("facilitatorError").innerText = "Please enter a valid facilator";
-        document.getElementById("facilitatorError").classList.remove("hidden");
-
-        // If there is still no invalid field, this will be to the first field to focus on
-        firstInvalidField = (firstInvalidField == "") ? facilitator : firstInvalidField;
-
-        // Set isValid to false, as this is not a valid input
-        isValid = false;
-    }
-
-    // If there is an invalid field, focus on this field
-    if (firstInvalidField != "") {
-        firstInvalidField.focus();
-    }
-
-    return isValid;
-}
+});
