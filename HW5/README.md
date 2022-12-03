e2b2c9159649416515c53ad638b3f724b1db6f03
Chantal Barrett <chantalbarrett@Chantals-Air.fios-router.home>
2022-12-03 15:47:18 -0500

    Homework five updates

---
+++ HW5/README.md
@@ -0,0 +1,30 @@
+# Assignment Fiv
+
+A website that fetches a JSON file.
+
+## Description
+
+This assignment uses HTML5, CSS, and JavaScript to create a web site that fetches a json file that contains the degrees I have recieved (note this JSON file only has two degrees, as I have only recieved a BS and am currently persuing my MS). 
+
+
+### How I Exceeded Requirements
+
+In trying my best to get an A on this assigment, I did the following:
+- Styled the page using CSS and made it responsive
+- Styled error messages written to the DOM
+- Made error messages different depending on the response status
+
+## Getting Started
+
+### Executing program
+
+* To run this program, simply navigate to: https://chantalbarrett.com/CS601/HW5/index.html (this is where the code for this assigment is hosted)
+
+## Author
+
+Chantal Barrett
+
+## Version History
+
+* 0.1
+    * Initial Release
--- HW5/script.js
+++ HW5/script.js
@@ -1,64 +1,105 @@
-let promise = Promise.resolve(function() {
-    let url = "https://chantalbarrett.com/CS601/HW5/degrees.json";
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type' : 'application/json'
-        }
-    });
-});
-
+// Declare and initalize needed variable
 const hiddenClass = "hidden";
 
 // Add event listener for fetching response
 document.getElementById("get-degrees").addEventListener("click", fetchDegrees);
 
-
+// Makes a request to degrees.json to get the degrees I have recieved
 function fetchDegrees() {
     const responseContainer = document.getElementById("response-container");
     const responseStatus = document.getElementById("response-status");
     const responseError = document.getElementById("response-error");
+    const degreesContainer = document.getElementById("degrees-container");
     
     // Create a fetch request to the JSON file that returns a promise
     let url = "https://chantalbarrett.com/CS601/HW5/degrees.json";
     fetch(url)
     .then(response => {
+        // Hide the welcome container
+        document.querySelector(".welcome-text").classList.add(hiddenClass);
+
+        // Display the response status
         if(response.ok) {
             responseStatus.innerText = "Yay! Your request returned a response status of " + response.status;
         }
         else {
             responseStatus.innerText = "Oh no! Your request returned a response status of " + response.status;
-            responseError.classList.remove(hiddenClass);
+            responseStatus.style.color = "var(--red)";
         }
         
         response.json().then(data => {
-            console.log(data);
-            const degreesContainer = document.getElementById("degrees-container");
-            // Clear any text thhat might be in the HTML
-            degreesContainer.innerHTML = "";
+            // Loop through the degrees
             let degrees = data.degrees;
             for (let i = 0; i < degrees.length; i++) {
-                let degree = degrees[i].degree;
+                let degree = degrees[i];
                 let major = degree.major;
                 let school = degree.school;
                 let type = degree.type;
                 let year = degree.year_conferred;
+                let courses = degree.courses;
 
-                let newHTMLElement = `<div class="degree">
-                                        <p class="school">School: ${school}</p>
-                                        <p class="major">Major: ${major}</p>
-                                        <p class="degree-type">Degree Type: ${type}</p>
-                                        <p class="year">Year Conferred: ${year}</p>
-                                    </div>`;
-                
-                degreesContainer.innerHTML += newHTMLElement;
+                // Determine the correct text to put as the year
+                // If the current year is before the year conferred
+                if (new Date().getFullYear() < year) {
+                    year = "Expected graduation: " + year;
+                }
+                else {
+                    year = "Degree recieved in: " + year;
+                }
+
+                // Create the element to hold all of this degree's information
+                const degreeElement = document.createElement("div");
+                degreeElement.className = "degree";
+
+                // Add the degree information to the element
+                degreeElement.innerHTML = `<div class="degree-title">
+                                                <img src="gradcap.png" width="50" height="50" alt="Graduation cap">
+                                                <p class="degree-type">${type} in ${major}</p>
+                                           </div>
+                                           <div class="degree-description">
+                                                <p class="school">School: ${school}</p>
+                                                <p class="year">${year}</p>
+                                           </div>`;
+
+                // Create the table for the courses
+                const courseTable = document.createElement("table");
+
+                // Add a caption to the table
+                const tableCaption = `<caption class="courses-header">Courses Taken</caption>`;
+                courseTable.innerHTML = tableCaption;
+
+                // Add a header to the table
+                const tableHeader = `<thead>
+                                        <tr>
+                                            <th>Course Number</th>
+                                            <th>Course Name</th>
+                                        </tr>
+                                    </thead>`;
+                courseTable.innerHTML += tableHeader;
+
+                // Loop through the courses and add each course to the table
+                for (let j = 0;  j < courses.length; j++) {
+                    let course = courses[j];
+                    const courseElement = document.createElement("tr");
+                    courseElement.innerHTML = `<td>${course.course_number}</td>
+                                               <td>${course.course}</td>`;
+                    courseTable.appendChild(courseElement);
+                }
+
+                // Add the course table to the degree element
+                degreeElement.appendChild(courseTable);
+
+                // Add the degree to the degrees container
+                degreesContainer.appendChild(degreeElement);
             }
 
+            // Show the degrees container
             degreesContainer.classList.remove(hiddenClass);
         })
     })
     .catch((error) => {
         // Show the error message
+        responseError.innerHTML = `Whoops! We were unable to retrieve the list of degrees<br>Error info: ${error.message}`;
         responseError.classList.remove(hiddenClass);
     });
 
