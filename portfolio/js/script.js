b7bbe6b9c87d3b3ddbed856b116eef9e758826cd
Chantal Barrett <chantalbarrett@Chantals-Air.fios-router.home>
2022-12-09 09:49:02 -0500

    Portfolio updates

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -7,32 +7,33 @@ document.addEventListener("DOMContentLoaded", function() {
         noscriptElements[i].classList.remove("noscript");
     }
 
-    // Event listeners for nav items on mobile
+    // Event listeners for nav items
     const navItems = document.querySelectorAll(".nav-item");
     const openClass = "nav-item-expanded";
     for (let i = 0; i < navItems.length; i++) {
         navItems[i].addEventListener("click", function(e) {
-            // e.currentTarget.nextElementSibling.style.display = "flex";
             let navItem = e.currentTarget;
             if (navItem.classList.contains(openClass)) {
                 navItem.classList.remove(openClass);
             }
             else {
                 navItem.classList.add(openClass);
-            }
-            
+            }         
         });
     }
 
     // Event listener for menu icon click
     document.querySelector(".hamburger").addEventListener("click", function() {
         // Show the nav
-        document.querySelector("nav").style.display = "block";
+        document.querySelector("nav").classList.add("nav-expanded");
+
+        // Add transition property to the nav
+        document.querySelector("nav").style.transition = "transform .3s ease-out";
     });
 
     // Event listener for close menu icon
     document.querySelector(".close-nav").addEventListener("click", function() {
         // Hide the nav
-        document.querySelector("nav").style.display = "none";
+        document.querySelector("nav").classList.remove("nav-expanded");
     });
 });
