e64b46b2548ad8e1417e27c20889bd0b522aa860
Chantal Barrett <chantalbarrett@Chantals-Air.fios-router.home>
2022-12-10 15:23:54 -0500

    Homework six

---
+++ HW6/README.md
@@ -0,0 +1,32 @@
+# Assignment Six
+
+A website that uses Vue.
+
+## Description
+
+This assignment uses HTML5, CSS, Vue.js, and JavaScript to create a web site that changes images on hover. It also allows the user to guess the difference in years of those images. 
+
+
+### How I Exceeded Requirements
+
+In trying my best to get an A on this assigment, I did the following:
+- Styled the page using CSS and made it responsive
+- Added functionality where the user can guess the differences in years between the images
+- Updated the DOM with the correct error message depending on the user's guess
+
+Note: The first time you hover on the image it may take a while to change, as the image is loaded on hover. I did this intentionally to help with page performace, however if I had more time I would consider a different implementation.
+
+## Getting Started
+
+### Executing program
+
+* To run this program, simply navigate to: https://chantalbarrett.com/CS601/HW6/index.html (this is where the code for this assigment is hosted).
+
+## Author
+
+Chantal Barrett
+
+## Version History
+
+* 0.1
+    * Initial Release
---
+++ HW6/script.js
@@ -0,0 +1,143 @@
+// Use Vue.js for page
+new Vue({
+    el: '#app',
+    data() {
+        return { 
+            people: [
+                {
+                    id: "chantal",
+                    name: "Chantal",
+                    youngPhotoSource: "./images/chantal_old.png",
+                    youngPhotoAlt: "Chantal when she was young",
+                    currentPhotoSource: "./images/chantal_current.png",
+                    currentPhotoAlt: "Chantal now",
+                    yearDifference: 25,
+                    maxAge: 27
+                },
+                {
+                    id: "kyle",
+                    name: "Kyle",
+                    youngPhotoSource: "./images/kyle_old.png",
+                    youngPhotoAlt: "Kyle when he was young",
+                    currentPhotoSource: "./images/kyle_current.png",
+                    currentPhotoAlt: "Kyle now",
+                    yearDifference: 7,
+                    maxAge: 25
+                },
+                {
+                    id: "chantal-kyle",
+                    name: "Chantal and Kyle",
+                    youngPhotoSource: "./images/chantal_kyle_old.png",
+                    youngPhotoAlt: "Chantal and Kyle when they were young",
+                    currentPhotoSource: "./images/chantal_kyle_current.png",
+                    currentPhotoAlt: "Chantal and Kyle now",
+                    yearDifference: 21,
+                    maxAge: 26
+                }
+            ]
+        };
+    },
+    methods: {
+        // Changes the picture on hover
+        changePicture: function(event) {
+            // Get the needed values from the current image
+            const image = event.currentTarget
+            let newImageSource = image.getAttribute("data-source"); // the new path of the image
+            let newImageAlt = image.getAttribute("data-alt"); // the new alt attribute for the image
+            let currentImageSource = image.getAttribute("src");
+            let currentImageAlt = image.getAttribute("alt");
+
+            // Update the image
+            image.setAttribute("src", newImageSource);
+            image.setAttribute("alt", newImageAlt);
+            image.setAttribute("data-source", currentImageSource);
+            image.setAttribute("data-alt", currentImageAlt);
+        },
+        checkYears: function(event) {
+            // Get the needed values
+            let id = event.currentTarget.getAttribute("data-guess-for");
+            let yearsGuess = parseInt(document.getElementById(id + "-years").value);
+            let result = document.getElementById(id + "-result");
+            let yearDifference, maxAge, name;
+            
+            // Get the actual years difference
+            for (let i = 0; i < this.people.length; i++) {
+                let person = this.people[i];
+                if (person.id === id) {
+                    yearDifference = person.yearDifference;
+                    maxAge = person.maxAge;
+                    name = person.name;
+                    break;
+                }
+            }
+
+            // If the user guessed the year difference correctly
+            if (yearsGuess === yearDifference) {
+                result.innerText = "Yay! You got it correct, these images are " + yearDifference + " years apart";
+            }
+            // If the user guessed a year differene thats older than the person/people in the photo
+            else if (maxAge < yearsGuess) {
+                result.innerText = "These photos are not that old! Try guessing again!";
+            }
+            else {
+                result.innerText = "Whoops, that's not correct. Try guessing again!";
+            }
+
+            // Show the result
+            result.classList.add("visible");
+        },
+        next: function(event) {
+            // Get the index of the person that is currently showing
+            let currentIndex = document.querySelector(".person-container.active").getAttribute("data-index");
+            let peopleLength = parseInt(document.querySelectorAll(".person-container").length) - 1;
+            let direction = event.currentTarget.getAttribute("data-direction");
+            let newIndex = "0";
+
+            // Get the new index
+            if (direction === "next") {
+                newIndex = parseInt(currentIndex) + 1;
+                if (newIndex > peopleLength) {
+                    newIndex = 0;
+                }
+            }
+            else if (direction === "previous") {
+                newIndex = parseInt(currentIndex) - 1;
+                if (newIndex === -1) {
+                    newIndex = peopleLength;
+                }
+            }
+
+            // Update the active container
+            document.querySelector(".person-container.active").classList.remove("active");
+            document.querySelectorAll(".person-container")[newIndex].classList.add("active");
+        }
+    },
+    template: `
+        <div id="app">
+                <section v-for="(person, index) in people" :id="person.id" :class="{'active': index === 0}" class="person-container" :data-index="index">
+                    <img class="image" :src="person.youngPhotoSource" :alt="person.youngPhotoAlt" :data-source="person.currentPhotoSource" :data-alt="person.currentPhotoAlt" width="370" height="370" @mouseover="changePicture" @mouseout="changePicture">
+
+                    <div class="input-container">
+                        <h2>How many years apart do you think these photos of {{ person.name }} were taken?</h2>
+                        <div class="input-wrapper">
+                            <input :id="person.id + '-years'" class="years-input" type="number" placeholder="0">
+                            <label :for="person.id + '-years'">years</label>
+                        </div>
+                        <button type="submit" class="submit-button" value="Guess" :data-guess-for="person.id" v-on:click="checkYears">Guess</button>
+                        <p :id="person.id + '-result'" class="result"> </p>
+                    </div>
+                </section>
+
+                <div class="navigation-container">
+                    <button class="previous arrow" data-direction="previous" v-on:click="next">
+                        <!-- For accessibility -->
+                        <span class="screen-reader-only">&larr; Previous</span>
+                    </button>
+                    <button class="next arrow" data-direction="next" v-on:click="next">
+                        <!-- For accessibility -->
+                        <span class="screen-reader-only">Next &rarr;</span>
+                </button>
+                </div>             
+        </div>
+    `,
+});
